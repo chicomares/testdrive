24b95f01c6fc7f41e44046e809b7890d597eba7b
chicomares <mareschico1@gmail.com>
2017-04-13 01:18:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-/* When the user clicks on the button, 
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+$('.drop-down-show-hide').hide();
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+$('#dropDown').change(function(){
+$(this).find("option").each(function()
+{
+$('#' + this.value).hide();
+});
+    $('#' + this.value).show();
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+});
